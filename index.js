394e9ed5f5392b093cc47e7a6aa952e8ab0be20c
rifai <alliiyassir@gmail.com>
2024-02-02 17:51:10 +0100

    Version 1.0

---
+++ index.js
@@ -0,0 +1,25 @@
+const numberBox = document.getElementById("numberBox");
+const radio_1 = document.getElementById("radio1");
+const radio_2 = document.getElementById("radio2");
+const result = document.getElementById("result");
+let temp;
+
+
+// Add event listener to the form to handle form submission
+document.querySelector("form").addEventListener("submit", function(event) {
+    event.preventDefault(); // Prevent the default form submission behavior
+    convert(); // Call the convert function
+});
+
+function convert() {
+    if (radio_1.checked) {
+        result.textContent = `${((numberBox.value * 9/5) + 32).toFixed(2)}°F`;
+    } else if (radio_2.checked) {
+        result.textContent = `${((numberBox.value - 32) * 5/9).toFixed(2)}°C`;
+    } else {
+        result.textContent = "Select a Unit";
+    }
+}
+
+
+
